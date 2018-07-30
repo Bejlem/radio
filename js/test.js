e49ca0e98e84880c8a77f18d869f152d6362e2a8
jubit <jub@mailbox.org>
2018-07-31 00:36:37 +0200

    Update test.js

--- js/test.js
+++ js/test.js
@@ -5,8 +5,8 @@
 var isMenuShown = false;
 var isMenuBig = true;
 var isHeaderBig = true;
-headerButtons = ['#menu_big_open', '#menu_big_close', '#menu_small_open', '#menu_small_close', '#header_button_open', '#header_button_close'];
-menu = [ '#menu_top', '#menu_side' ];
+headerButtons = ['#menu_open', '#menu_close', '#header_button_open', '#header_button_close'];
+menu = [ '#menu_top', '#menu_side', '#menu_bot', '#menu_social' ];
 const colormap = ['yellow', 'green', 'blue', 'dark', 'black33'];
 
 function onPointerDown(event){
@@ -40,14 +40,19 @@ toggleContent = function () {
         // Opening Header
         //window.removeEventListener( 'scroll', No_Scroll );
         $('header').animate({height: '100vh'});
-        $(headerButtons[4]).animate({top:'-80'}, 300);
-        $(headerButtons[5]).animate({bottom:'7'}, 700);
+        $(headerButtons[2]).animate({top:'-80'}, 300);
+        $(headerButtons[3]).animate({bottom:'7'}, 700);
     } else {
         // Closing Header
         //window.addEventListener( 'scroll', No_Scroll );
+        $('#social_links_top').animate({bottom: '68'}, 0);
         $('header').animate({height:'64'});
-        $(headerButtons[4]).animate({top:'3'}, 700);
-        $(headerButtons[5]).animate({bottom:'-80'}, 300);
+        $(headerButtons[2]).animate({top:'3'}, 700);
+        $(headerButtons[3]).animate({bottom:'-80'}, 300);
+        $(menu[2]).animate({bottom: '-30', height: '55'}, 10); //
+        $('#social_links_bot').animate({top: '12'}, 200); //
+
+        $(menu[1]).animate({left: '-50', width: '0'}, 355);
     }
     isHeaderBig = !isHeaderBig;
     //alert('Is the header big = ' + isHeaderBig );
@@ -63,37 +68,57 @@ toggleBigMenu = function () {
     }
 }
 
-toggleMenu = function () {
+toggleMenu = function (id) {
+    //alert(id);
     if( isMenuShown ) {
         if( isHeaderBig ) {
             //alert('Hide big Menu');
-            $('#menu_links').animate({left: '-160'}, 450);
-            $(menu[0]).animate({left: '-2', top: '-100', height: '0'}, 10);
-            $(menu[1]).animate({left: '-50', width: '0'}, 10);
-            $(headerButtons[0]).animate({left: '24', opacity: '1'}, 200);
-            $(headerButtons[1]).animate({left: '-60'}, 50);
-            //$('#menu_options').animate({left: '-200'}, {duration: 10, queue: false});
-            //$('#menu_options').animate({bottom: '-60', left: '0'}, {duration: 500, queue: true});
-            $('#option_links').animate({bottom: '-60'}, 450);
+            if( id != 'menu_big_open') {
+                $('#social_links_top').animate({bottom: '68'}, 10);
+                $('#menu_links_side').animate({left: '-160'}, 450);
+                $('#option_links').animate({bottom: '-60'}, 450);
+                $(menu[0]).animate({top: '-100', height: '0'}, 10);
+                $(menu[1]).animate({left: '-50', width: '0'}, 10);
+                $(headerButtons[0]).animate({left: '24', opacity: '1'}, 200);
+                $(headerButtons[0]).css('cursor', 'pointer');
+                $(headerButtons[1]).animate({left: '-60'}, 50);
+                //$('#menu_options').animate({left: '-200'}, {duration: 10, queue: false});
+                //$('#menu_options').animate({bottom: '-60', left: '0'}, {duration: 500, queue: true});
+            }
 
         } else {
-            alert('Hide small Menu');
+            //alert('Hide small Menu');
+            $('#social_links_bot').animate({top: '54'}, 10); //
+            $(menu[0]).animate({top: '-100', height: '0'}, 10);
+            $(menu[2]).animate({bottom: '-100', height: '0'}, 10); //
+            $(headerButtons[0]).animate({left: '24', opacity: '1'}, 200);
+            $(headerButtons[0]).css('cursor', 'pointer');
+            $(headerButtons[1]).animate({left: '-60'}, 50);
 
         }
     } else{
         if( isHeaderBig ) {
             //alert('Show big Menu');
-            $(menu[0]).animate({left: '0', top: '-32', height: '66'}, 10);
-            $(menu[1]).animate({left: '-2', width: '160'}, 100);
+            $(menu[0]).animate({top: '-32', height: '66'}, 10);
+            $(menu[1]).animate({left: '0', width: '160'}, 100);
             //$(headerButtons[0]).animate({top: '-40'}, 10);
             $(headerButtons[0]).animate({left: '140', opacity: '0'}, 10);
+            $(headerButtons[0]).css('cursor', 'default');
             $(headerButtons[1]).animate({left: '24'}, 100);
 
-            $('#menu_links').animate({left: '-15'}, 1200);
-            $('#option_links').animate({bottom: '12'}, 200)
-        } else {
-            alert('Show small Menu');
+            $('#social_links_top').animate({bottom: '16'}, 200);
+            $('#menu_links_side').animate({left: '-15'}, 1200);
+            $('#option_links').animate({bottom: '12'}, 200);
 
+        } else {
+            //alert('Show small Menu');
+            $(menu[0]).animate({top: '-32', height: '66'}, 10);
+            $(menu[2]).animate({bottom: '-30', height: '55'}, 10); //
+            $(headerButtons[0]).animate({left: '140', opacity: '0'}, 10);
+            $(headerButtons[0]).css('cursor', 'default');
+            $(headerButtons[1]).animate({left: '24'}, 100);
+
+            $('#social_links_bot').animate({top: '12'}, 200); //
         }
     }
     isMenuShown = !isMenuShown;
